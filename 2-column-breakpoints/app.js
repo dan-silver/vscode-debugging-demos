861e772e4a55fa30370a058ae0ef2497de3ad276
Dan Silver <dannysilver3@gmail.com>
2017-03-30 11:43:15 +0200

    Multiple variables on one line

--- 2-column-breakpoints/app.js
+++ 2-column-breakpoints/app.js
@@ -1,5 +1,8 @@
 // use shift F9 to set column breakpoints
 // conditional when i == 50
+
+let a = 5; let b = 100; let c = 75; let d = "a";
+
 for (let i=0; i< 100; i++) {
     console.log("Hello, world")
 }
