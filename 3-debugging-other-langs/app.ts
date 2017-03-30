6b775ba45037d623fea776a151270bb81cb4cb94
Dan Silver <dannysilver3@gmail.com>
2017-03-30 11:46:54 +0200

    Update ts example

--- 3-debugging-other-langs/app.ts
+++ 3-debugging-other-langs/app.ts
@@ -3,8 +3,11 @@
 // set breakpoint either with 'debugger' or in gutter 
 let i:number = 0;
 setInterval(() => {
-    // debugger
     i++;
 
     console.log(`hello world! ${i}`)
 }, 300);
+
+
+// delete .js.map file
+// make change, .map file needs to stay in sync
